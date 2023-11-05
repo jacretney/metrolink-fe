a08c52460ee5cf37036095769f43f559e818d4a2
Jim <jacretney@gmail.com>
2023-11-05 19:04:30 +0000

    Improve UX with loading states

---
+++ src/components/LoadingEllipsis.tsx
@@ -0,0 +1,7 @@
+import './LoadingEllipsis.style.css';
+
+const LoadingEllipsis = () => {
+    return <span className="loading" />
+}
+
+export default LoadingEllipsis;
--- src/components/TramTimetable.tsx
+++ src/components/TramTimetable.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+
 import { TramStopResponse } from "../data/TramStopResponse";
 import { Tram } from "../data/Tram";
+import LoadingEllipsis from "./LoadingEllipsis";
 
 function getDueTime(status: string, minutes: number): string 
 {
@@ -15,21 +17,37 @@ function getDueTime(status: string, minutes: number): string
 function TramTimetable() {
     const [trams, setTrams] = useState<Tram[]>([]);
     const [message, setMessage] = useState('');
+    const [errorCount, setErrorCount] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
             axios.get<TramStopResponse>('/stops/129036')
                 .then(async response => {
                     const { data } = response.data;
-
+                    
+                    setErrorCount(0);
                     setTrams(data.trams);
                     setMessage(data.message ?? '');
-                });
+                })
+                .catch(error => {
+                    setErrorCount(prev => prev + 1);
+                })
         }, 5 * 1000);
 
         return () => clearInterval(interval);
     }, []);
 
+    if (errorCount > 0) {
+        return (
+            <p>There was an error fetching tram stop data, retrying ({errorCount})<LoadingEllipsis /></p>
+        )
+    }
+
+    if (! trams.length) {
+        return (
+            <p>Fetching tram stop data<LoadingEllipsis /></p>
+        )
+    }
   
     return (
         <div className="table">
